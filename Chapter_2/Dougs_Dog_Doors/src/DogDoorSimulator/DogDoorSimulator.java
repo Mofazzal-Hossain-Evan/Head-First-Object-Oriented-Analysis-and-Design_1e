b29457e8c807b315a005d6a45374ba064d37bfc7
Evan <md.mofazzalhossain18@gmail.com>
2024-11-19 23:57:55 +0600

    chapter-2-done

--- Chapter_2/Dougs_Dog_Doors/src/DogDoorSimulator/DogDoorSimulator.java
+++ Chapter_2/Dougs_Dog_Doors/src/DogDoorSimulator/DogDoorSimulator.java
@@ -8,6 +8,9 @@ public class DogDoorSimulator {
         DogDoor door = new DogDoor();
         Remote remote = new Remote(door);
 
+//        System.out.println("Fido barks to go outside  ");
+//        remote.pressButton();
+
         System.out.println("Fido barks to go outside  ");
         remote.pressButton();
 
@@ -17,7 +20,15 @@ public class DogDoorSimulator {
         System.out.println("\nFido all done  ");
        // remote.pressButton();
 
+        try{
+            Thread.currentThread().sleep(10000);
+        } catch (InterruptedException e) {}
+
+
+        System.out.println("..but he is stuck outside! ");
+        System.out.println("..\nFido starts barking...");
+        System.out.println("..so Ginna grabs the remote control ");
+        remote.pressButton();
         System.out.println("\nFido has back inside  ");
-       // remote.pressButton();
     }
 }
