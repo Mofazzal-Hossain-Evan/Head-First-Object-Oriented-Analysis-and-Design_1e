685ce3d86149b450b6f4af0a2d336cb2e4fe6296
Evan <md.mofazzalhossain18@gmail.com>
2024-11-19 22:45:46 +0600

    Test drive, version 2.0

---
+++ Chapter_2/Dougs_Dog_Doors/src/DogDoorSimulator/DogDoorSimulator.java
@@ -0,0 +1,23 @@
+package DogDoorSimulator;
+
+
+
+public class DogDoorSimulator {
+
+    public static void main (String[] args) {
+        DogDoor door = new DogDoor();
+        Remote remote = new Remote(door);
+
+        System.out.println("Fido barks to go outside  ");
+        remote.pressButton();
+
+        System.out.println("\nFido has gone outside  ");
+       // remote.pressButton();
+
+        System.out.println("\nFido all done  ");
+       // remote.pressButton();
+
+        System.out.println("\nFido has back inside  ");
+       // remote.pressButton();
+    }
+}
